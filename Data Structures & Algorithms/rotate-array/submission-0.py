6ead1d059f44a847544ba2fa400c4ffe81554f40
Roee Sivan <roee.sivan@post.runi.ac.il>
2026-08-31 13:08:03 +0300

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if n == 0:
+            return
+        k %= n
+        nums[:] = nums[-k:] +nums[:-k]
+
+
+
+    
+        

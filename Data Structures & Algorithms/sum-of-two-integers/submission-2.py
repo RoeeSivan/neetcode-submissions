9b5501351cbf28f947ee949c7b78126717780959
Roee Sivan <roee.sivan@post.runi.ac.il>
2026-09-07 12:23:35 +0300

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        while b & mask:
+        # Calculate the carry bits
+            carry = (a & b) << 1
+        # Calculate the sum without carry
+            a = a ^ b
+        # Move the carry to b for the next iteration
+            b = carry
+        # If b is greater than 0, it means we have a positive result or 
+        # the carry didn't clear within 32 bits due to Python's unbounded ints.
+        return (a & mask) if b > 0 else a

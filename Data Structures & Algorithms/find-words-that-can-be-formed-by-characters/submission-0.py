724d5d3cf6aca1cbccf5744c708647d1710e5144
Roee Sivan <roee.sivan@post.runi.ac.il>
2026-09-17 13:26:27 +0300

    Add: find-words-that-can-be-formed-by-characters - submission-0

---
+++ Data Structures & Algorithms/find-words-that-can-be-formed-by-characters/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        wordict = Counter(chars)
+        res = 0
+        for word in words:
+            is_valid = True
+            dic = Counter(word) # the specific dictionary for the word
+            for char in dic:
+                if dic[char] > wordict[char]:
+                    is_valid = False
+                    break
+            if is_valid:
+                res += len(word)
+        return res
+        

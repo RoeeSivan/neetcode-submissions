b8a0bc39f6779de3d153d891c7f8863bd54aec25
Roee Sivan <roee.sivan@post.runi.ac.il>
2026-09-21 14:17:10 +0300

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path = [] #current permutation
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path.copy())
+                return
+            for num in nums:
+                if num in path:
+                    continue
+                path.append(num)
+                backtrack(path)
+                path.pop()
+        backtrack(path)
+        return res
